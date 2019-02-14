f85aae0ba41a1578df8673e5ef8562399eb184e3
Danil Garmanov <blazern@yandex-team.ru>
2019-02-14 12:07:44 +0300

    Try to avoid DB containers race on first start

--- db-for-tests/second-db-related-init.sh
+++ db-for-tests/second-db-related-init.sh
@@ -4,5 +4,9 @@ set -e
 # Ensure the path to live db is set.
 test $LIVE_DB_PATH
 
+export SLEEP_TIME=10
+echo "DB-tests container going to sleep for $SLEEP_TIME seconds"
+sleep $SLEEP_TIME
+
 # Copy live db.
 cp -r $LIVE_DB_PATH/* /var/lib/postgresql/data/
