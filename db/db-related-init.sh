fbfd58c8ebf98d9ee7e229c586ca6ceb1a9e4295
Danil Garmanov <blazern@yandex-team.ru>
2019-11-27 11:07:56 +0300

    Use built server binaries instead of building them
    https://trello.com/c/9YsBFyci

--- db/db-related-init.sh
+++ db/db-related-init.sh
@@ -5,10 +5,8 @@ test $USERS_PASSWORD
 
 createuser recipe_calculator_server
 createuser recipe_calculator_client
-createuser recipe_calculator_admin
 createdb recipe_calculator_main
-psql -c "GRANT ALL ON schema public TO recipe_calculator_admin;"
-psql -c "ALTER USER recipe_calculator_admin WITH SUPERUSER;"
-psql -c "ALTER USER recipe_calculator_admin with encrypted password '$USERS_PASSWORD'";
+psql -c "GRANT ALL ON schema public TO recipe_calculator_server;"
+psql -c "ALTER USER recipe_calculator_server WITH SUPERUSER;"
 psql -c "ALTER USER recipe_calculator_server with encrypted password '$USERS_PASSWORD'";
 psql -c "ALTER USER recipe_calculator_client with encrypted password '$USERS_PASSWORD'";
